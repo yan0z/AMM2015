79391216f00543d6f9d70c445bb511e1d12e7be5
amm <amm@amm-VirtualBox>
2015-03-18 15:05:05 +0100

    secondo aggiornamento

--- FPAMM2015/index.php
+++ FPAMM2015/index.php
@@ -1,16 +1,95 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <title>FP.AMM</title>
+        <meta name="keywords" content="HTML">
+        <meta name="Author" content="Stefano Carta 48554">
+        <meta name="description" content="Progetto finale AMM">
+        <link rel="stylesheet" type="text/css" href="Css/style.css" media="screen">
     </head>
     <body>
-        <p>merda</p>
+        <div id="PageIndex">
+            <div id="HeaderIndex">
+                <h1>Progetto Finale AMM</h1>
+            </div>      <!--Chiusura HeaderIdex-->
+            
+            <div id="BodyIndex">
+                <div id="DescriptionIndex">
+                    <p>
+                        <h4>Questo progetto mette a disposizione un sito internet
+                            per la compra/vendita di libri di testo.</h4>
+			<br/>
+                    </p>
+                    <hr/>
+                    <p>
+                        <h4>Per ogni articolo ventuto si mettono a disposizione le seguenti informazioni:</h4><br/>
+                        <ul>
+                            <li>
+                                <h4>Titolo del libro</h4>
+                            </li>
+                            <li>	
+                                <h4>Autore</h4>
+                            </li>
+                            <li>
+                                <h4>Casa Editrice</h4>
+                            </li>
+                            <li>	
+                                <h4>Breve descrizione della trama</h4>
+                            </li>
+                            <li>	
+                                <h4>Genere del libro</h4>
+                            </li>
+                            <li>	
+                                <h4>Anno Pubblicazione</h4>
+                            </li>
+                            <li>	
+                                <h4>Disponibilità del prodotto</h4>
+                            </li>
+                            <li>	
+                                <h4>Prezzo</h4>
+                            </li>
+                        </ul>
+                    </p>
+                    <p>
+                        <h4>Per ogni utente vengono gestite diverse funzionalit&agrave;.</h4><br/>
+			Gli utenti possono essere:<br/>
+			<ul>
+                            <li>
+				<h4>ADMIN</h4>
+                            </li>
+                            <li>	
+				<h4>Normal USER</h4>
+                            </li>
+                            <li>
+				<h4>USER/Venditore</h4>
+                            </li>
+			</ul>
+                    </p>
+                    <hr/>		
+                </div>           <!--Chiusura DescriptionIndex-->
+                    
+                <div id="AccessIndex">  
+                    <p id="Accedi">
+                        <a href="indexHome.html">ACCEDI AD INDEX</a>
+                    </p>
+                </div>           <!--Chiusura AccessIndex-->
+                    
+                <div id="InfoIndex">
+                    <h4>Progetto a cura di:</h4>
+                    <br/>
+                    <ul>
+                        <li>Nome e Cognome: Stefano Carta</li>
+                        <li>Numero Matricola: 48554</li>
+                    </ul>
+                </div>           <!--Chiusura InfoIndex-->
+            </div>               <!--Chiusura BodyIndex-->
+                
+            <div id="BottomIndex">
+                
+            </div>               <!--Chiusura BottomIndex-->
+        </div> 
+        <!--Chiusura PageIndex-->
         <?php
         // put your code here
         
