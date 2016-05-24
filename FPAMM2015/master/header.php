106f6aec4ec609ce536504a6a413042a097d9ecf
amm <amm@amm-VirtualBox>
2016-05-24 12:28:02 +0200

    new repository master

--- FPAMM2015/master/header.php
+++ FPAMM2015/master/header.php
@@ -3,8 +3,8 @@
     <head>
         <title>BeB -AMM</title> 
         <meta charset="UTF-8"/>
-        <meta name="description" content="Progetto finale: B&B-AMM"/>
         <meta name="author" content="Stefano Carta"/>
+        <meta name="description" content="Progetto finale: B&B-AMM"/>
 	<link rel="stylesheet" type="text/css"  href="../css/finalStyle.css" />
         <script type="text/javascript" src="../js/jquery-1.12.4.min.js"></script>
     </head>
