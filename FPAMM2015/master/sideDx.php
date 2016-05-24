7b9b87dfce03111b3439eafb0f31f3b327a11c1f
amm <amm@amm-VirtualBox>
2016-05-24 12:50:07 +0200

    new repository master

--- FPAMM2015/master/sideDx.php
+++ FPAMM2015/master/sideDx.php
@@ -1,26 +1,43 @@
 <div id="menu-sx">
-    <?php if(isset($_SESSION['logged']) == false): ?>
-        <p> <strong>B&B-AMM</strong></p>
-    <?php endif;?>
 
-    <?php if(isset($_SESSION['logged'])): ?>
-        <p><strong>Bentornato:</br></strong> <?=$_SESSION['username'];?></p>
-        
-        <?php if($_SESSION['livello'] == "admin"): ?>
-            <ul>  
-                <li><a href="../adminFunction/deleteBooking.php"><strong>Cancella Prenotazioni</strong></a></li> 
-                <li><a href="../adminFunction/serchBooking.php"><strong>Ricerca Prenotazioni</strong></a></li> 
-            </ul>
-        
-        <?php else: ?>
-            <ul>
-                <li><a href="../userFunction/booking.php"><strong>Prenota</strong></a></li>
-                <li><a href="../userFunction/viewBooking.php"><strong>Riepilogo prenotazioni</strong></a></li>
-            </ul>  
+        <?php if(isset($_SESSION['logged']) == false): ?>
+
+            <p> <strong>Benvenuto al B&B Villa Serena</strong> </p>
+
+
         <?php endif;?>
-    <?php endif;?>
- </div>
 
+
+        <?php if(isset($_SESSION['logged'])): ?>
+
+   
+
+	            <p><strong>Bentornato:</br></strong> <?=$_SESSION['username'];?></p>
+
+	            <p><strong>Grado:</br></strong> <?=$_SESSION['livello'];?></p>
+
+
+	        
+                <?php if($_SESSION['livello'] == "admin"): ?>
+
+                    <ul>  
+                        <li><a href="../adminFunction/serchBooking.php"><strong>Cerca Prenotazioni</strong></a></li> 
+                    <li><a href="../adminFunction/deleteBooking.php"><strong>Cancella Prenotazioni</strong></a></li> 
+                    </ul>
+
+
+        <?php else: ?>
+
+                <ul>
+                    <li><a href="../userFunction/prenota.php"><strong>Prenota</strong></a></li>
+                    <li><a href="../userFunction/visualizzaPrenotazioni.php"><strong>Le mie Prenotazioni</strong></a></li>
+                </ul>
+               
+        <?php endif;?>
+        <?php endif;?>
+     <ul>   
+</ul>
+ </div>
 
  
 
