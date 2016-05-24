c889749ebeee1fc2ade3875508ac92246428fa02
amm <amm@amm-VirtualBox>
2016-05-24 11:25:41 +0200

    new repository master

--- FPAMM2015/master/header.php
+++ FPAMM2015/master/header.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8"/>
         <meta name="description" content="B&B-AMM"/>
         <meta name="author" content="Stefano Carta"/>
-	<link rel="stylesheet" type="text/css"  href="../css/finalStyle.css" />
+	<link rel="stylesheet" type="text/css"  href="../Css/finalStyle.css" />
         <script type="text/javascript" src="../js/jquery-1.9.1.min.js"></script>
     </head>
 </html>
