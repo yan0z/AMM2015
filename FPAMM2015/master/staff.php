38ae01a225e705f543843efccaabb4d2f944aadb
amm <amm@amm-VirtualBox>
2016-05-25 12:54:02 +0200

    new repository master

--- FPAMM2015/master/staff.php
+++ FPAMM2015/master/staff.php
@@ -13,27 +13,27 @@
                 <div class="box">
                     <h2>Staff B&B - AMM</h2>
                     <img src="../images/boss.jpg" class="staff" alt="Boss">
+                    Il Boss - Stefano
                     <img src="../images/cuoco.jpg" class="staff" alt="cuoco">
                     <img src="../images/cameriera.jpg" class="staff" alt="cameriera">
                     <img src="../images/giardiniere.jpg" class="staff" alt="giardiniere">
                     <img src="../images/reception.png" class="staff" alt="receptionist">
-                        <h3>Contatti</h3></br>
-                        <li>
-                            <ul>
-                                Via bellissima n. 100
-                            </ul>
-                            <ul>
-                                Tel. 1234567890
-                            </ul>
-                            <ul>
-                                Cell. 3401111111
-                            </ul>
-                            <ul>
-                                Email. infoAMM@hotmail.it
-                            </ul>
-                        </li>
                     
-
+                    <h3>Contatti</h3></br>
+                    <ul>
+                        <li>
+                            Via: Bellissima n. 100
+                        </li>
+                        <li>
+                            Tel: 1234567890
+                        </li>
+                        <li>
+                            Cel: 3401234567
+                        </li>
+                        <li>
+                            Email: emailbebAmm@hotmail.it
+                        </li>
+                    </ul>
 		</div>
             </div>
 	</div>
