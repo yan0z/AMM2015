4a0f488d8b818a48afce2c27b6f21f8a5fcfcb45
amm <amm@amm-VirtualBox>
2016-05-25 11:55:31 +0200

    new repository master

--- FPAMM2015/master/master.php
+++ FPAMM2015/master/master.php
@@ -12,6 +12,7 @@
                 <div class="box">
                     <h2>Il B&B - AMM</h2>
                     <img src="../images/casa.jpg" class="illustrazione" alt="Giardino Villa Serena">
+                    <img src="../images/casa.jpg" class="illustrazione" alt="Giardino Villa Serena">
                     <p>Situato in uno dei migliori quartieri di Cagliari ma lontano dal frastuono cittadino.<br/>
                        La villa, in stile moderno/rustico, offre tutto quello che un visitatore può sperare.
                        Anche se risulta vicino al centro, e' in una zona tranquilla dove i nostri visitatori possono godersi il sole e il cielo di Cagliari magari a bordo piscina.
