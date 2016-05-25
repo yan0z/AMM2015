534f0fb419918846d3cbde12075884dff2459255
amm <amm@amm-VirtualBox>
2016-05-25 13:01:07 +0200

    new repository master

--- FPAMM2015/master/staff.php
+++ FPAMM2015/master/staff.php
@@ -12,13 +12,18 @@
             <div id="contentx">
                 <div class="box">
                     <h2>Staff B&B - AMM</h2>
-                    <img src="../images/boss.jpg" class="staff" alt="Boss">
-                    Il Boss - Stefano
+                    <img src="../images/boss.jpg" class="staff" alt="boss">
                     <img src="../images/cuoco.jpg" class="staff" alt="cuoco">
                     <img src="../images/cameriera.jpg" class="staff" alt="cameriera">
                     <img src="../images/giardiniere.jpg" class="staff" alt="giardiniere">
                     <img src="../images/reception.png" class="staff" alt="receptionist">
-                    
+                    <ul id="staff" >
+                        <li><strong>Il boss - Stafano</strong></li>
+                        <li><strong>Il cuoco - Pippo</strong></li>
+                        <li><strong>La governante - Gina</strong></li>
+                        <li><strong>Il giardiniere - Rocco</strong></li>
+                        <li><strong>La receptionist - Lina</strong></li>
+                    </ul>
                     <h3>Contatti</h3></br>
                     <ul>
                         <li>
