86b326a279713f2a6cf7a06f58f5da0d42abe48a
yan0z <stefanocarta90@hotmail.it>
2016-10-20 18:24:48 +0200

    ultime modifiche nuovo progetto14

--- FPAMM2015/php/functionUser/visualizzaPrenotazione.php
+++ FPAMM2015/php/functionUser/visualizzaPrenotazione.php
@@ -9,46 +9,48 @@ include_once basename(__DIR__) . '/../controller/UserController.php';
             <div class="box">
                 <p>In questa sezione puoi vedere lo storico delle tue prenotazioni. E' permessa la cancellazione.</p>
                 <div class="box2">
-                <table class="tblprenotazioneintestazione">
-                    <tr>
-                        <td>Nome</td>
-                        <td>Cognome</td>
-                        <td>Email</td>
-                        <td>Arrivo</td>
-                        <td>Partenza</td>
-                        <td>Cancella</td>
-                    </tr>
-                </table>
-                <?php
-                //conto quante occorrenze sono presenti nell'array $prenotazioni
-                $num = count($prenotazioni);
-                //se sono presenti prenotazioni allora procediamo a stamparla
-                if(count($prenotazioni)>0){ ?>
-                <table class="tblprenotazione">
-                    <?php
-                    for($i=0; $i<$num; $i++){  ?>
-                        <tr> 
-                        <?php 
-                        for($j=1; $j<6; $j++){ ?>
-                            <td><?php echo $prenotazioni[$i][$j];?></td>
-                        <?php } ?>
-                            <td><a href="index.php?page=user&subpage=visualizzaPrenotazione&cmd=cancellaPrenotazione&id=<?= $idPre=$prenotazioni[$i][0] ?><?= $vd->scriviToken('&') ?>" title="cancella prenotazione">
-                                    <img src="../images/no.png" class="comando" alt="Elimina" >
-                                </a>
-                            </td>
+                    <table class="tblprenotazioneintestazione">
+                        <tr>
+                            <td>Nome</td>
+                            <td>Cognome</td>
+                            <td>Email</td>
+                            <td>Arrivo</td>
+                            <td>Partenza</td>
+                            <td>Cancella</td>
                         </tr>
-                    <?php } ?>
-                </table>
-                <?php
-                //non sono presenti prenotazioni
-                }
-                else
-                {?> 
-                    <div>Non è presente nessuna prenotazione.</div>
-                <?php
-                
-                }
-                ?>
+                    </table>
+                    <div class="visulizza">  
+                        <?php
+                        //conto quante occorrenze sono presenti nell'array $prenotazioni
+                        $num = count($prenotazioni);
+                        //se sono presenti prenotazioni allora procediamo a stamparla
+                        if(count($prenotazioni)>0){ ?>
+                        <table class="tblprenotazione">
+                            <?php
+                            for($i=0; $i<$num; $i++){  ?>
+                                <tr> 
+                                <?php 
+                                for($j=1; $j<6; $j++){ ?>
+                                    <td><?php echo $prenotazioni[$i][$j];?></td>
+                                <?php } ?>
+                                    <td><a href="index.php?page=user&subpage=visualizzaPrenotazione&cmd=cancellaPrenotazione&id=<?= $idPre=$prenotazioni[$i][0] ?><?= $vd->scriviToken('&') ?>" title="cancella prenotazione">
+                                            <img src="../images/no.png" class="comando" alt="Elimina" >
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </table>
+                        <?php
+                        //non sono presenti prenotazioni
+                        }
+                        else
+                        {?> 
+                            <div>Non è presente nessuna prenotazione.</div>
+                        <?php
+
+                        }
+                        ?>
+                     </div>
                 </div>
             </div>
 	</div>
