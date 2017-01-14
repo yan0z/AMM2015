d19e6c52d6d71ec0093f6dd9b2f6df5ede9497ee
yan0z <stefanocarta90@hotmail.it>
2017-01-14 20:54:11 +0100

    last commit

--- FPAMM2015/php/view/master.php
+++ FPAMM2015/php/view/master.php
@@ -1,111 +1,87 @@
 <?php
 include_once basename(__DIR__) .'ViewDescriptor.php';
 include_once basename(__DIR__) . '/../Settings.php';
+?>
+<!DOCTYPE html>
+<!-- 
+     pagina master, contiene tutto il layout della applicazione 
+     le varie pagine vengono caricate a "pezzi" a seconda della zona
+     del layout:
+     - logo (header)
+     - menu (navigation bar)
+     - content (la parte centrale con il contenuto)
+     - footer (footer)
 
-if (!$vd->isJson()) {
-    ?>
-    <!DOCTYPE html>
-    <!-- 
-         pagina master, contiene tutto il layout della applicazione 
-         le varie pagine vengono caricate a "pezzi" a seconda della zona
-         del layout:
-         - logo (header)
-         - menu (navigation bar)
-         - content (la parte centrale con il contenuto)
-         - footer (footer)
+      Queste informazioni sono manentute in una struttura dati, chiamata ViewDescriptor
+      la classe contiene anche le stringhe per i messaggi di feedback per 
+      l'utente (errori e conferme delle operazioni)
+-->
+<html>
+    <head>
+        <title>B&B AMM</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <base href="<?= Settings::getApplicationPath() ?>php/"/>
+        <meta name="keywords" content="B&B AMM" />
+        <link href="../css/finalStyle.css" rel="stylesheet" type="text/css" media="screen" />
+    </head>
+    <body>
+        <div id="page">
+            <header>
+                <!--  header -->
+                <?php 
+                    $headerImage = $vd->getHeaderImage();
+                    require "$headerImage";
+                ?>
 
-          Queste informazioni sono manentute in una struttura dati, chiamata ViewDescriptor
-          la classe contiene anche le stringhe per i messaggi di feedback per 
-          l'utente (errori e conferme delle operazioni)
-    -->
-    <html>
-        <head>
-            <title>B&B AMM</title>
-            <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-            <base href="<?= Settings::getApplicationPath() ?>php/"/>
-            <meta name="keywords" content="B&B AMM" />
-            <link href="../css/finalStyle.css" rel="stylesheet" type="text/css" media="screen" />
-        </head>
-        <body>
-            <div id="page">
-                <header>
-                    <!--  header -->
-                    <?php 
-                        $headerImage = $vd->getHeaderImage();
-                        require "$headerImage";
-                    ?>
-                    
+                <?php
+                    $navigationBar = $vd->getNavigationBar();
+                    require "$navigationBar";
+                ?>
+            </header>
+
+            <div id="content"> 
+                <?php
+                    if($vd->getSottoPagina() == 'servizi'){
+                        $servizi = $vd->getServizi();
+                        require "$servizi";
+                    }
+                    else if($vd->getSottoPagina() == 'staff'){
+                        $staff = $vd->getStaff();
+                        require "$staff";
+                    }
+                    else if($vd->getSottoPagina() == 'about'){
+                        $about = $vd->getAbout();
+                        require "$about";
+                    }
+                    else if($vd->getSottoPagina() == 'prenota'){
+                        $prenota = $vd->getBooking();
+                        require "$prenota";
+                    }
+                    else if($vd->getSottoPagina() == 'visualizzaPrenotazione'){
+                        $visualizzaPrenotazione = $vd->getViewBooking();
+                        require "$visualizzaPrenotazione";
+                    }
+                    else if($vd->getSottoPagina() == 'visualizzaPrenotazioneAdmin'){
+                        $visualizzaPrenotazioneAdmin = $vd->getViewBookingAdmin();
+                        require "$visualizzaPrenotazioneAdmin";
+                    }
+                    else
+                    {
+                        $content = $vd->getContent();
+                        require "$content";
+                    }  
+                ?>
+            </div>
+            <div class="clear"></div>
+            <!--  footer -->
+            <footer>
+                <div id="footer">
                     <?php
-                        $navigationBar = $vd->getNavigationBar();
-                        require "$navigationBar";
-                    ?>
-                </header>
-                
-                <div id="content"> 
-                    <?php
-                        if($vd->getSottoPagina() == 'servizi'){
-                            $servizi = $vd->getServizi();
-                            require "$servizi";
-                        }
-                        else if($vd->getSottoPagina() == 'staff'){
-                            $staff = $vd->getStaff();
-                            require "$staff";
-                        }
-                        else if($vd->getSottoPagina() == 'about'){
-                            $about = $vd->getAbout();
-                            require "$about";
-                        }
-                        else if($vd->getSottoPagina() == 'prenota'){
-                            $prenota = $vd->getBooking();
-                            require "$prenota";
-                        }
-                        else if($vd->getSottoPagina() == 'visualizzaPrenotazione'){
-                            $visualizzaPrenotazione = $vd->getViewBooking();
-                            require "$visualizzaPrenotazione";
-                        }
-                        else if($vd->getSottoPagina() == 'visualizzaPrenotazioneAdmin'){
-                            $visualizzaPrenotazioneAdmin = $vd->getViewBookingAdmin();
-                            require "$visualizzaPrenotazioneAdmin";
-                        }
-                        else
-                        {
-                            $content = $vd->getContent();
-                            require "$content";
-                        }  
+                        $footer = $vd->getFooter();
+                        require "$footer";
                     ?>
                 </div>
-                <div class="clear"></div>
-                <!--  footer -->
-                <footer>
-                    <div id="footer">
-                        <?php
-                            $footer = $vd->getFooter();
-                            require "$footer";
-                        ?>
-                    </div>
-                </footer>
-            </div>
-        </body>
-    </html>
-    <?php
-} else {
-
-    header('Cache-Control: no-cache, must-revalidate');
-    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-    header('Content-type: application/json');
-    
-    $json = array();
-    $json["id"] = $vd->getNavigationBar();
-    $json["content"] = $vd->getContent();
-    $json["msg"] = $msg;
-    echo json_encode($json);
-}
-
-
-
-
-
-
-
-
-
+            </footer>
+        </div>
+    </body>
+</html>
